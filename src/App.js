deb3a64fa71376a9edc961bde48b4299eda60706
ferhatcklt <ferhatcklt@gmail.com>
2021-08-11 17:05:03 +0300

    React ile fake tweet generator uygulamasını için tweet tasarımı yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.scss';
+import './icons';
+import { LikeIcon, ReplyIcon, RetweetIcon, ShareIcon, VerifiedIcon } from './icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="tweet-settings">
+      <h3>Tweet Ayarları</h3>
     </div>
+      <div className="tweet-container">
+        <div className="tweet">
+          <div className="tweet-author">
+            <img src="https://pbs.twimg.com/profile_images/1421390859858235398/O20MKpKX_normal.jpg" alt="" />
+            <div>
+              <div className="name">Haluk Levent <VerifiedIcon /></div>
+              <div className="username">@haluklevent</div>
+            </div>
+          </div>
+          <div className="tweet-content">
+            <p>
+            Yangınlarda zarar gören ihtiyaç sahipleri için 
+            Serkan Buyruk ve
+            Özgür Uludağ kardeşlerimiz
+            50 şer bin TL destekte bulundular.
+
+            Teşekkürler canlarım!
+            
+            </p>
+          </div>
+          <div className="tweet-stats">
+            <span><b>24</b> Retweet</span>
+            <span><b>24</b> Alıntı Tweetler</span>
+            <span><b>24</b> Beğeni</span>
+          </div>
+          <div className="tweet-actions">
+            <span><ReplyIcon /></span>
+            <span><RetweetIcon /></span>
+            <span><LikeIcon /></span>
+            <span><ShareIcon /></span>
+          </div>
+        </div>
+      </div>
+    </>
+    
   );
 }
 
