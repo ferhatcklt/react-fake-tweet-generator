66e9d88c714dc481ef72843740e5ab58c26dcd9b
ferhatcklt <ferhatcklt@gmail.com>
2021-08-13 17:37:34 +0300

    Eklenen kütüphaneler: material-ui, html2canvas ve use-react-screenshot. Oluştur butonu için tweeti png indirme eklendi.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import Q from 'q';
-import React, {useState} from 'react';
+import React, {useState, createRef, useEffect} from 'react';
 import './App.scss';
 import { LikeIcon, ReplyIcon, RetweetIcon, ShareIcon, VerifiedIcon } from './components/icons';
+import {AvatarLoader} from './components/loaders';
+import {useScreenshot} from 'use-react-screenshot';
 
 const tweetFormat = tweet => {
   tweet = tweet
   .replace(/@([\w]+)/g,'<span>@$1</span>')
   .replace(/#([\wşçöğıİÜÖÇŞĞ]+)/gi,'<span>#$1</span>')
-  .replace(/(https?:\/\/[\w\.\/]+)/,'<span>$1</span>')
+  .replace(/(https?:\/\/[\w\.\/]+)/gi,'<span>$1</span>')
   return tweet;
 };
 
@@ -21,6 +22,8 @@ const formatNumber = number => {
 };
 
 function App() {
+  const tweetRef = createRef(null);
+  const downloadRef = createRef();
   const [name, setName] = useState();
   const [username,setUsername] = useState();
   const [isVerified, setIsVerified] = useState(false);
@@ -29,6 +32,22 @@ function App() {
   const [retweets, setRetweets] = useState(0);
   const [quoteTweets, setQuoteTweets] = useState(0);
   const [likes, setLikes] = useState(0);
+  const [image,takeScreenshot ] = useScreenshot();
+  const getImage = () => takeScreenshot(tweetRef.current);
+  useEffect(() => {
+    if(image){
+      downloadRef.current.click();
+    }
+  }, [image]);
+  const avatarHandle = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.addEventListener('load', function(){
+      setAvatar(this.result);
+    });
+    reader.readAsDataURL(file);
+  };
+
 
   return (
     <>
@@ -38,18 +57,22 @@ function App() {
           <label>Ad Soyad</label><li><input type="text" className="input"  value={name} onChange={e => setName(e.target.value)} /></li>
           <label>Kullanıcı Adı</label><li><input type="text" className="input" value={username} onChange={e => setUsername(e.target.value)}/></li>
           <label>Tweet</label><li><textarea type="text" className="input" value={tweet} onChange={e => setTweet(e.target.value)}  maxLength="290" /></li>
+          <label>Avatar</label><li><input type="file" className="input"  onChange={avatarHandle}   /></li>
           <label>Retweet</label><li><input type="text" className="input"  value={retweets} onChange={e => setRetweets(e.target.value)}/></li>
           <label>Alıntı Tweet</label><li><input type="text" className="input" value={quoteTweets} onChange={e => setQuoteTweets(e.target.value)}/></li>
           <label>Beğeni</label><li><input type="text" className="input"  value={likes} onChange={e => setLikes(e.target.value)}/></li>
-          <button>Oluştur</button>
+          <label>Onaylı Hesap</label><li><input type="checkbox" className="input"  value={isVerified} onChange={e => setIsVerified(e.target.value)}/></li>
+          
+          <button onClick={getImage}>Oluştur</button>
+          <a href={image} download="tweet.png" ref={downloadRef} style={{display:"none"}}>Tweeti indir</a>
         </ul>
       </div>
       <div className="tweet-container">
-        <div className="tweet">
+        <div className="tweet" ref={tweetRef}>
           <div className="tweet-author">
-            <img src="https://pbs.twimg.com/profile_images/1421390859858235398/O20MKpKX_normal.jpg" alt="" />
+            {(avatar && <img src={avatar} alt="" />) || <AvatarLoader />}
             <div>
-              <div className="name">{name || 'Ad Soyad'} {isVerified && <VerifiedIcon />} </div>
+              <div className="name">{name || 'Ad Soyad'} {isVerified === false || <VerifiedIcon />} </div>
               <div className="username">@{username || 'kullaniciadi'}</div>
             </div>
           </div>
